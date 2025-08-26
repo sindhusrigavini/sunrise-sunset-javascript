8fab428a031f06ad8c8c59c70c666dd4059d6b54
Sindhu Sri Gavini <sindhusrigavini@gmail.com>
2025-08-26 12:42:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// function sunset(){
+//     let suns=document.getElementById('sun').src='https://media1.tenor.com/images/ceeac567fa9b9d7c17af3b5969bae31d/tenor.gif?itemid=8632998'
+// }
+// function sunrise(){
+//     document.getElementById('sun').src='https://media.giphy.com/media/13DobtLzCTj16M/giphy.gif'
+// }
+function sunset() {
+    document.body.style.backgroundImage = "url('https://media1.tenor.com/images/ceeac567fa9b9d7c17af3b5969bae31d/tenor.gif?itemid=8632998')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+    
+}
+function sunrise() {
+    document.body.style.backgroundImage = "url('https://media.giphy.com/media/13DobtLzCTj16M/giphy.gif')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+    
+}
